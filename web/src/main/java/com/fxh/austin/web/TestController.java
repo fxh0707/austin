0987cd714c46b42b28ad992555a8dd1f8d69cb60
fxh <599014549@qq.com>
2022-09-03 21:58:46 +0800

    初始化项目

---
+++ web/src/main/java/com/fxh/austin/web/TestController.java
@@ -0,0 +1,14 @@
+package com.fxh.austin.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/hello")
+    private String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s", name);
+    }
+}
