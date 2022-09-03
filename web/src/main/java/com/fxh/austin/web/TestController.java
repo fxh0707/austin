17001664770e93917f6953591eab930e0fb79643
fxh <599014549@qq.com>
2022-09-03 23:42:18 +0800

    add logback

--- web/src/main/java/com/fxh/austin/web/TestController.java
+++ web/src/main/java/com/fxh/austin/web/TestController.java
@@ -1,5 +1,7 @@
 package com.fxh.austin.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    private final Logger logger = LoggerFactory.getLogger(TestController.class);
+
     @RequestMapping("/hello")
     private String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        logger.error("error logback for austin");
+        logger.info("info logback for austin");
         return String.format("Hello %s", name);
     }
 }
